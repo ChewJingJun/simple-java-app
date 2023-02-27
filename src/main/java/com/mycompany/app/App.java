c664515e2bf885f453fcbcbee6342aac2c929beb
Chew Jing Jun <jing-jun.chew@keysight.com>
2023-02-27 07:56:35 +0000

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("ABC DEFG HIJK");
+        System.out.println("UNKNOWN");
     }
 
     public String getMessage() {
