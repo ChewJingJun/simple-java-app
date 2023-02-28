cc344764fdc4e28d1e3835a6652e14435b4b33c4
Chew Jing Jun <jing-jun.chew@keysight.com>
2023-02-28 05:44:53 +0000

    Make a change to pipeline job to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("UNKNOWN");
+        System.out.println("It is a hot afternoon");
     }
 
     public String getMessage() {
